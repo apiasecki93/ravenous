ac34ed46c2b7f65dfcfb69a4e58cd975dc978bab
Artur Piasecki <apiasecki93@gmail.com>
2020-12-21 11:31:56 +0100

    ravenoue

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import './App.css'; 
-import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
+import BusinessList from '../BusinessList/BusinessList';
 
-import logo from './logo.svg';
-import './App.css';
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
 
-function App() {
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business,
+];
+
+class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        {searchBar}
-        {businessList}
+        <SearchBar />
+        <BusinessList businesses={businesses} />
       </div>
-    )
+    );
   }
 }
 
